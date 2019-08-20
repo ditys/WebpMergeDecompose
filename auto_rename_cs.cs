ef2ab2903756f4fd38ac6e266b54644b3a0efe48
Anest <anest@66ws.cc>
2019-08-20 00:05:18 -0400

    add auto rename cs

---
+++ auto_rename_cs.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO;
+
+namespace cstemp
+{
+    class Program
+    {
+        public static void folder_to_file(string path)
+        {
+            string[] dirs = Directory.GetDirectories(path);
+            foreach (string s in dirs)
+            {
+                folder_to_file(s);
+            }
+            string[] files = Directory.GetFiles(path);
+            foreach (string s in files)
+            {
+                Directory.Move(s, "./" + s.Substring(2).Replace("/", "_").Replace("\\", "_"));
+            }
+            Console.WriteLine(path + "is finished.");
+        }
+        public static int Main()
+        {
+            Console.Title = "Auto Rename cs";
+            string[] dirs = Directory.GetDirectories("./");
+            foreach (string s in dirs)
+            {
+                Console.WriteLine(s);
+                folder_to_file(s);
+            }
+            Console.WriteLine("\n\nAll Finished.");
+            Console.Read();
+            return 0;
+        }
+    }
+}
