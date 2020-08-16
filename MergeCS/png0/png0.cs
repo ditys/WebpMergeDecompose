f58e40b4f6aa8bc45d8fb8d20158422aaf0cd24b
Anest <anest@66ws.cc>
2020-08-16 08:29:57 -0400

    add png0

---
+++ MergeCS/png0/png0.cs
@@ -0,0 +1,41 @@
+﻿using ImageMagick;
+using System;
+using System.Collections.Concurrent;
+using System.IO;
+namespace WMD
+{
+    namespace png0
+    {
+        class png0
+        {
+            static void Main(string[] args)
+            {
+                ConcurrentQueue<worker.WorkItem> q = new ConcurrentQueue<worker.WorkItem>();
+                string[] files = Directory.GetFiles("png0");
+                foreach (string theFile in files)
+                {
+                    q.Enqueue(new worker.WorkItem { Name = theFile, IsFile = true });
+                }
+                worker.worker.work(5, delegate () { Task(q); });
+            }
+            static void Task(ConcurrentQueue<worker.WorkItem> q)
+            {
+                worker.WorkItem workItem;
+                while (q.Count != 0 && q.TryDequeue(out workItem))
+                {
+                    if (workItem.IsFile)
+                    {
+                        using (MagickImage image = new MagickImage(workItem.Name))
+                        {
+                            string temp = "resultPng0/" + Path.GetFileName(workItem.Name);
+                            image.Quality = 100;
+                            image.Write(temp);
+                            Console.WriteLine(temp);
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
