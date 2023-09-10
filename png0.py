2aa0c42ed288987f6bbb75898cf7037d1a85a86f
Anest <32736719+AnestLarry@users.noreply.github.com>
2023-09-09 23:28:47 -0400

    feat(png0): add multiprocessing

--- png0.py
+++ png0.py
@@ -1,12 +1,28 @@
 from PIL import Image
 import os
+import multiprocessing
 try:
 	os.mkdir("Result")
 except:
 	print("create Result Error")
-ld = os.listdir()
-ld = [x for x in ld if x[-4:] in (".png",".PNG")]
-for i in ld:
-	f = Image.open(i)
-	f.save("./Result/"+i,compress_level = 0)
-	f.close()
+
+def Worker(fs: multiprocessing.Queue, all_task: int):
+	while not fs.empty():
+		i = fs.get()
+		f = Image.open(i)
+		f.save("./Result/"+i,compress_level = 0)
+		f.close()
+
+if __name__ == "__main__":
+	multiprocessing.freeze_support()
+	ld = [x for x in os.listdir() if x[-4:] in (".png",".PNG")]
+	files = multiprocessing.Queue()
+	
+	for i in ld:
+		files.put(i)
+	processList = []
+	for _ in range(3):
+		processList.append(multiprocessing.Process(target=Worker,args=(files,files.qsize())))
+		processList[-1].start()
+	for i in processList:
+		i.join()
